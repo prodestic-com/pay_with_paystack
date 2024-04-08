c8b9953f81b5cc2de98c2dafec13d263984fdde7
AloneProdestic <armelmatio@gmail.com>
2024-04-08 14:01:13 +0000

    fixed subcription

--- lib/pay_with_paystack.dart
+++ lib/pay_with_paystack.dart
@@ -2,6 +2,7 @@ library pay_with_paystack;
 
 import 'package:flutter/material.dart';
 import 'package:pay_with_paystack/src/paystack_pay_now.dart';
+import 'package:pay_with_paystack/src/paystack_pay_subscribe.dart';
 
 /// Main class, use the [now] method and provide arguments like;
 /// secret [secretKey], [reference], [currency], [email], [email], [paymentChannel] and [amount].
@@ -47,6 +48,62 @@ class PayWithPayStack {
                 secretKey: secretKey,
                 email: customerEmail,
                 reference: reference,
+                currency: currency,
+                amount: amount,
+                paymentChannel: paymentChannel,
+                metadata: metaData,
+                transactionCompleted: transactionCompleted,
+                transactionNotCompleted: transactionNotCompleted,
+                callbackUrl: callbackUrl,
+              )),
+    );
+  }
+
+  Future<dynamic> subscribe({
+    /// Context provided from current view
+    required BuildContext context,
+
+    /// Secret key is provided from your paystack account
+    required String secretKey,
+
+    /// Email of the customer
+    required String customerEmail,
+
+    /// Alpha numeric and/or number ID to a transaction
+    required String reference,
+
+    /// Alpha numeric and/or number plan you to subscribe to
+    required String plan,
+
+    /// callBack URL to handle redirection
+    required String callbackUrl,
+
+    /// Currency of the transaction
+    required String currency,
+
+    /// Amount you want to charge the user. Add extra two zeros after typing the amount
+    required String amount,
+
+    /// What happens next after transaction is completed
+    required Function? transactionCompleted,
+
+    /// What happens next after transaction is not completed
+    required Function? transactionNotCompleted,
+
+    /// Extra data not consumed by Paystack but for developer purposes
+    Object? metaData,
+
+    /// Payment Channels you want to make available to the user
+    Object? paymentChannel,
+  }) {
+    return Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => PaystackPaySubscribe(
+                secretKey: secretKey,
+                email: customerEmail,
+                reference: reference,
+                plan: plan,
                 currency: currency,
                 amount: amount,
                 paymentChannel: paymentChannel,
